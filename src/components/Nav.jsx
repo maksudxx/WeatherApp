a64ed06a66495ffa2f03d104fe8d54a90de47921
facundomaksud@gmail.com <facundomaksud@gmail.com>
2021-12-13 16:49:47 -0300

    estilos arregleados navbar y searchbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import SearchBar from './SearchBar.jsx';
-import styles from './Nav.module.css'
-import {Link} from 'react-router-dom';
+import React from "react";
+import SearchBar from "./SearchBar.jsx";
+import styles from "./Nav.module.css";
+import { Link } from "react-router-dom";
 
-
-function Nav({onSearch}) {
+function Nav({ onSearch }) {
+  const mql = window.matchMedia("(max-width: 600px)");
   return (
-    <nav className={styles.container}>
-      <Link to="/" className={styles.link}>
-        <span className={styles.title}>Weather App</span>
-      </Link>
-      <SearchBar onSearch={onSearch} />
-      <Link to="/about" className={styles.link}>
-        <span>About</span>
-      </Link>
-    </nav>
+   
+      <nav className={styles.container}>
+        <Link to="/" className={styles.link}>
+          <span className={styles.title}>Weather App</span>
+        </Link>
+        <Link to="/about" className={`${styles.link} ${styles.order}`}>
+          <span>About</span>
+        </Link>
+        <SearchBar onSearch={onSearch}/>
+      </nav>
+    
   );
-};
+}
 
 export default Nav;
